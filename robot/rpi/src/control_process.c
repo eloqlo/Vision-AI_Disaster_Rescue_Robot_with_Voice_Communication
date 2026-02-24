e25c83183729fab64a9e70ef3c86513b7232e542
eloqlo <eloqlo.jh@gmail.com>
2026-02-24 15:23:21 +0900

    debug: RPI SPI 통신 CE0핀 활성화 기능 추가

--- robot/rpi/src/control_process.c
+++ robot/rpi/src/control_process.c
@@ -1,24 +1,40 @@
-// compile: $ gcc control_process.c -lgpiod
 /*
+compile: 
+    $ gcc control_process.c -lgpiod
+
 TODO List:
     1. main loop에서 socket, motor, GPIO IRQ thread로 분리하여 처리해보기 (26.02.23 ~ )
 */
+#define DEBUG
+#define VERSION "1.0"
+
+
 #include <stdio.h>
-#include <stdint.h>         // uint8_t, uint32_t 등 고정 크기 정수형 정의
-#include <unistd.h>         // UNIX 표준 함수 (read, write, close 등)
-#include <gpiod.h>          // Linux GPIO 제어 라이브러리 (libgpiod)
-#include <json-c/json.h>    // JSON 처리 라이브러리 (json-c)
-#include <stdatomic.h>        // atomic 변수 사용을 위한 헤더
+#include <unistd.h>
+#include <stdint.h>
+#include <gpiod.h>
+#include <stdatomic.h>
+#include <stdlib.h>
+
+#include <fcntl.h>          
+#include <sys/ioctl.h>         
+#include <linux/spi/spidev.h> 
 #include <pthread.h>
 
-#define SET 1
-#define CLEAR 0
+#include <json-c/json.h>    // JSON 처리 라이브러리 (json-c)
 
+#define SET     1
+#define CLEAR   0
+#define SPI_MODE        SPI_MODE_0
+#define SPI_BITS        8
+#define SPI_SPEED       1000000 // 1 MHz
+
+int spi_fd = -1;
 int sonar_irq_flag = CLEAR; // 초음파 센서 인터럽트 플래그
 int spi_irq_flag = CLEAR;   // SPI 인터럽트 플래그
 atomic_int sonar_rising_flag = 0;     // 동기화를 위해 stdatomic 사용(C11 이상)
 atomic_int sonar_falling_flag = 0;
-atomic_int spi_falling_flag = 0;
+atomic_int spi_rising_flag = 0;
 
 uint8_t spi_buffer[7];
 
@@ -60,10 +76,10 @@ struct gpiod_line_request* initialize_gpio(const char *chip_path) {
     gpiod_line_settings_set_edge_detection(settings_sonar, GPIOD_LINE_EDGE_BOTH);
     gpiod_line_config_add_line_settings(line_cfg, &sonar_offset, 1, settings_sonar);
 
-    // Pin33 (SPI) 설정: Input + Falling Edge
+    // Pin33 (SPI) 설정: Input + Rising Edge
     settings_spi = gpiod_line_settings_new();
     gpiod_line_settings_set_direction(settings_spi, GPIOD_LINE_DIRECTION_INPUT);
-    gpiod_line_settings_set_edge_detection(settings_spi, GPIOD_LINE_EDGE_FALLING);
+    gpiod_line_settings_set_edge_detection(settings_spi, GPIOD_LINE_EDGE_RISING);
     gpiod_line_config_add_line_settings(line_cfg, &spi_offset, 1, settings_spi);
 
     // request 설정, 라인 요청
@@ -78,26 +94,46 @@ struct gpiod_line_request* initialize_gpio(const char *chip_path) {
     gpiod_request_config_free(req_cfg);
     gpiod_chip_close(chip);
 
+    // SPI CE0핀 제어 위해 Pin18을 a0(alternative function 0)으로 설정
+    int result = system("pinctrl set 18 a0");
+    if (result == -1) {
+        // system 함수 자체의 호출 실패
+        printf("GPIO 초기화중 SPI1 CE0핀 초기화 실패.\n");
+    }
+
     return request;
 }
 
 int initialize_spi() {
-    // TODO SPI 초기화 코드 작성
-    printf("SPI Initialized.\n");
+    const char *device = "/dev/spidev10.0";  // SPI10: 센서 허브 데이터 받기
+    uint8_t mode = SPI_MODE; 
+    uint8_t bits = SPI_BITS;
+    uint32_t speed = SPI_SPEED; // 일단 1 MHz
+    
+    spi_fd = open(device, O_RDWR);
+    if (spi_fd < 0) {
+        perror("Failed to open SPI device");
+        return EXIT_FAILURE;
+    }
 
+    ioctl(spi_fd, SPI_IOC_WR_MODE, &mode);
+    ioctl(spi_fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+    ioctl(spi_fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+    
+    printf("SPI Initialized: %s at %d Hz\n", device, speed);
     return EXIT_SUCCESS;
 }
 
 int initialize_server() {
     // TODO 소켓 초기화 코드 작성
-    printf("Server Initialized.\n");
+    printf("Server 미구현.\n");
 
     return EXIT_SUCCESS;
 }
 
 int initialize_motors() {
     // TODO 모터 초기화 코드 작성
-    printf("Motors Initialized.\n");
+    printf("Motors 미구현.\n");
 
     return EXIT_SUCCESS;
 }
@@ -113,14 +149,42 @@ void sonar_irq_callback(int polarity) {
     else {
         printf("Sonar Falling Edge Detected.\n");
     }
-
 }
 
 void spi_irq_callback() {
     // Interrupt 우선순위 낮음
-    // TODO SPI 인터럽트 처리 코드 작성
+    if (spi_fd < 0) {
+        perror("SPI device not initialized\n");
+        return;
+    }
 
-    printf("SPI IRQ Callback executed.\n");
+    uint8_t rx[7] = {0,};
+    uint8_t tx_dummy[7] = {0,};
+
+    struct spi_ioc_transfer tr = {
+        .tx_buf = (unsigned long)tx_dummy,
+        .rx_buf = (unsigned long)rx,
+        .len = 7,
+        .delay_usecs = 0,
+        .speed_hz = SPI_SPEED,
+        .bits_per_word = SPI_BITS,      // word당 비트 수
+    };
+
+    if (ioctl(spi_fd, SPI_IOC_MESSAGE(1), &tr) < 1) {
+        perror("Can't sent SPI message\n");
+        return;
+    }
+
+    // 읽어온 데이터 처리
+    for (int i = 0; i < 7; i++) {
+        spi_buffer[i] = rx[i];
+    }
+
+#ifdef DEBUG
+    printf("SPI Read: %02X %02X %02X %02X %02X %02X %02X\n", 
+            rx[0], rx[1], rx[2], rx[3], rx[4], rx[5], rx[6]);
+#endif
+
 }
 
 
@@ -130,7 +194,7 @@ int main() {
     /* Process 생성 */
     //TODO Vison Process 생성
     //TODO Audio Process 생성
-    
+    printf(" - Control Process Version %s - \n", VERSION);
     printf("------------- Control Process Started! -------------\n");
 
     /* Initialization */
@@ -172,7 +236,7 @@ int main() {
         // TODO 모터 제어 코드 작성
 
         // 3. GPIO 인터럽트 처리
-        if (gpiod_line_request_wait_edge_events(request, -1) > 0) {
+        if (gpiod_line_request_wait_edge_events(request, -1) > 0) {     //TODO: param(-1) 무한대기 therad로 분리해서 반드시 수정하기
             int num_events = gpiod_line_request_read_edge_events(request, buffer, 16);
 
             for (int i=0; i<num_events; i++) {
@@ -185,21 +249,24 @@ int main() {
                     else if (type == GPIOD_EDGE_EVENT_FALLING_EDGE) sonar_falling_flag = SET;
                 }
                 else if (offset == 13) { // Pin33 (SPI IRQ)
-                    if (type == GPIOD_EDGE_EVENT_FALLING_EDGE) spi_falling_flag = SET;
+                    if (type == GPIOD_EDGE_EVENT_RISING_EDGE) spi_rising_flag = SET;
                 }
             }
         }
         if (sonar_rising_flag) {
             sonar_rising_flag = CLEAR;
-            sonar_irq_callback(1);
+            sonar_irq_callback(1);  // 1: Rising Edge
         }
         if (sonar_falling_flag) {
             sonar_falling_flag = CLEAR;
-            sonar_irq_callback(0);
+            sonar_irq_callback(0);  // 0: Falling Edge
         }
-        if (spi_falling_flag) {
-            spi_falling_flag = CLEAR;
+        if (spi_rising_flag) {
+            spi_rising_flag = CLEAR;
+            printf("SPI Falling Edge Detected.\n");
+            gpiod_line_request_set_value(request, 18, GPIOD_LINE_VALUE_ACTIVE);    // SPI CE0을 LOW로 설정하여 SPI 통신 시작
             spi_irq_callback();
+            gpiod_line_request_set_value(request, 18, GPIOD_LINE_VALUE_INACTIVE);  // SPI CE0을 HIGH로 설정하여 SPI 통신 종료
         }
     }// Main Loop End
 
